f1eaff9f75689be248d80b6b45368bd98d53eb1e
DavidDinkevich <david.dinkevich@gmail.com>
2017-07-27 13:27:05 -0400

    Updated documentation

--- Proofer/src/geometry/proofs/Figure.java
+++ Proofer/src/geometry/proofs/Figure.java
@@ -6,6 +6,12 @@ import java.util.List;
 public interface Figure {
 	public String getName();
 	public void setName(String name);
+	
+	/**
+	 * Get whether the given name is a valid name
+	 * for this figure. NOTE: it does not have to EQUAL
+	 * the name of this figure, it just must be valid.
+	 */
 	public boolean isValidName(String name);
 	
 	public List<Figure> getChildren();
