26e9a786261b94bba7adaf9411351e1c1d854294
DavidDinkevich <david.dinkevich@gmail.com>
2017-09-15 19:11:32 -0400

    Added support for identifying hidden triangles

--- Proofer/src/geometry/proofs/Preprocessor.java
+++ Proofer/src/geometry/proofs/Preprocessor.java
@@ -2,10 +2,8 @@ package geometry.proofs;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.AbstractMap.SimpleEntry;
 
 import geometry.Vec2;
 import geometry.shapes.Angle;
@@ -146,10 +144,12 @@ public class Preprocessor {
 	 * included in the diagram and can be safely referenced.
 	 * @param diagram the diagram that contains the hidden figures.
 	 */
+	@SuppressWarnings("unchecked")
 	private void addHiddenFigures(Diagram diagram) {
+		List<Angle> hiddenAngles = Collections.emptyList();
 		final int COUNT = diagram.getFigures().size();
 		
-		for (int i = 0, lastCheckedTri = i-1; i < COUNT; i++, lastCheckedTri++) {
+		for (int i = 0, lastCheckedTri = i-1; i < COUNT; i++) {
 			// If the element's shape is a Triangle
 			if (diagram.getFigures().get(i) instanceof Triangle) {
 				// Get the Triangle
@@ -162,81 +162,118 @@ public class Preprocessor {
 						continue;
 					// If the second element's shape is a Triangle
 					if (diagram.getFigures().get(j) instanceof Triangle) {
+						// Update last checked triangle
+						lastCheckedTri = j;
 						// Get the second element's shape
 						Triangle tri1 = (Triangle)diagram.getFigures().get(j);
-						// Add straight lines to diagram's list of elements
-						for (Vertex sharedVertex : getSharedVertices(tri0, tri1)) {
-							SimpleEntry<Collection<Figure>, Collection<Figure>> figures =
-									identifyHiddenFigures(tri0, tri1, sharedVertex);
-							diagram.addFigures(figures.getKey());
-							diagram.addFigures(figures.getValue());
-						}
+						// Add hidden figures
+						List<List<? extends Figure>> hiddenFigures =
+								identifyHiddenSegsAndAngles(diagram, tri0, tri1);
+						// Hidden segments
+						diagram.addFigures(hiddenFigures.get(0));
+						// Hidden angles
+						hiddenAngles = (List<Angle>)hiddenFigures.get(1);
+						diagram.addFigures(hiddenAngles);
 					}
 				}
 			}
 		}
+		
+		// Add hidden triangles
+		for (Figure a : hiddenAngles) {
+			String originalAngleName = a.getName();
+			// Vertices
+			String sharedVert = originalAngleName.substring(1, 2);
+			String v0 = originalAngleName.substring(0, 1);
+			String v1 = originalAngleName.substring(2); 
+			String secondAngle = sharedVert + v0 + v1;
+			String thirdAngle = sharedVert + v1 + v0;
+			
+			// Check if the two derived angles (secondAngle, thirdAngle) exist in the diagram
+			if (!diagram.containsFigures(Arrays.asList(secondAngle, thirdAngle), Angle.class))
+				continue;
+			// New triangle
+			Angle originalAngle = (Angle)a;
+			Vertex vertex0 = (Vertex) originalAngle.getChild(v0);
+			Vertex vertex1 = (Vertex) originalAngle.getChild(v1);
+			Vertex shared = (Vertex) originalAngle.getChild(sharedVert);
+			Triangle triangle = new Triangle(vertex0, shared, vertex1);
+			System.err.println(triangle.getName());
+		}
 	}
 	
-	private SimpleEntry<Collection<Figure>, Collection<Figure>>
-		identifyHiddenFigures(Triangle tri0, Triangle tri1, Vertex sharedVertex) {
-		Collection<Figure> segs = new ArrayList<>();
-		Collection<Figure> angles = new ArrayList<>();
+	/**
+	 * Identify hidden {@link Segment}s and {@link Angle}s between two {@link Triangle}s
+	 * in the given {@link Diagram}
+	 * @param diag the Diagram
+	 * @param tri0 the first Triangle
+	 * @param tri1 the second Triangle
+	 * @param sharedVertex the 
+	 * @return two Lists, the first holding Segments and the second Angles
+	 */
+	private List<List<? extends Figure>> identifyHiddenSegsAndAngles(
+			Diagram diag, Triangle tri0, Triangle tri1) {
+		List<Segment> segs = new ArrayList<>();
+		List<Angle> angles = new ArrayList<>();
 		
-		// Loop through segments in first triangle
-		for (Segment seg0 : tri0.getSides()) {
-			// If the segment DOES NOT contain the shared vertex (given)
-			if (!seg0.getName().contains(sharedVertex.getName()))
-				continue;
-			// Go through segments of second triangle
-			for (Segment seg1 : tri1.getSides()) {
-				// If segment in second triangle does NOT contains the shared vertex
-				if (!seg1.getName().contains(sharedVertex.getName()))
+		// For all shared vertices between the two triangles
+		for (Vertex sharedVertex : getSharedVertices(tri0, tri1)) {
+			System.out.println("Time");
+			// Loop through segments in first triangle
+			for (Segment seg0 : tri0.getSides()) {
+				// If the segment DOES NOT contain the shared vertex (given)
+				if (!seg0.getName().contains(sharedVertex.getName()))
 					continue;
-				// If we're analyzing the same segment, we can't combine it
-				if (seg1.equals(seg0))
-					continue;
-				// Check if the two segments are parallel
-				// Get slope of segment 0
-				Slope seg0Slope = seg0.getSlope();
-				// Get slope of segment 1
-				Slope seg1Slope = seg1.getSlope();
-				/*
-				 * Compare slopes: if the slopes are the same, add a new hidden segment.
-				 * Otherwise, add the hidden angle.z
-				 */
-				if (seg0Slope.equals(seg1Slope)) {
-					// Combine segments
-					
-					// Vertices of both segments in one list
-					List<Vertex> segVerts = new ArrayList<>(Arrays.asList(seg0.getVertices()));
-					segVerts.addAll(Arrays.asList(seg1.getVertices()));
-					// ---------------------
-					// Vertices of new segment--farthest apart
-					Vertex[] newSegVerts = getFarthestVertices(segVerts);
-					
-					// The new, combined straight line
-					Segment newStraightLine = new Segment(newSegVerts);
-					segs.add(newStraightLine);
-				}
-				// Add hidden angles
-				else {
-					String angleName = Utils.getAngleBetween(seg0.getName(), seg1.getName());
-					String unsharedVert0 = angleName.substring(0, 1);
-					String unsharedVert1 = angleName.substring(2);
-					String sharedVert = angleName.substring(1, 2);
-					Vertex first, second, third;
-					first = (Vertex) (seg0.containsChild(unsharedVert0) ?
-							seg0.getChild(unsharedVert0) : seg0.getChild(unsharedVert1));
-					third = (Vertex) (first.getNameChar() == unsharedVert0.charAt(0) ?
-							seg1.getChild(unsharedVert1) : seg1.getChild(unsharedVert0));
-					second = (Vertex) seg0.getChild(sharedVert);
-					Angle newAngle = new Angle(first, second, third);
-					angles.add(newAngle);
-					System.err.println(newAngle.getName());
+				// Go through segments of second triangle
+				for (Segment seg1 : tri1.getSides()) {
+					// If segment in second triangle does NOT contains the shared vertex
+					if (!seg1.getName().contains(sharedVertex.getName()))
+						continue;
+					// If we're analyzing the same segment, we can't combine it
+					if (seg1.equals(seg0))
+						continue;
+					// Check if the two segments are parallel
+					// Get slope of segment 0
+					Slope seg0Slope = seg0.getSlope();
+					// Get slope of segment 1
+					Slope seg1Slope = seg1.getSlope();
+					/*
+					 * Compare slopes: if the slopes are the same, add a new hidden segment.
+					 * Otherwise, add the hidden angle.z
+					 */
+					if (seg0Slope.equals(seg1Slope)) {
+						// Combine segments
+						
+						// Vertices of both segments in one list
+						List<Vertex> segVerts = new ArrayList<>(Arrays.asList(seg0.getVertices()));
+						segVerts.addAll(Arrays.asList(seg1.getVertices()));
+						// ---------------------
+						// Vertices of new segment--farthest apart
+						Vertex[] newSegVerts = getFarthestVertices(segVerts);
+						
+						// The new, combined straight line
+						Segment newStraightLine = new Segment(newSegVerts);
+						segs.add(newStraightLine);
+					}
+					// Add hidden angles
+					else {
+						String angleName = Utils.getAngleBetween(seg0.getName(), seg1.getName());
+						String unsharedVert0 = angleName.substring(0, 1);
+						String unsharedVert1 = angleName.substring(2);
+						String sharedVert = angleName.substring(1, 2);
+						Vertex first, second, third;
+						first = (Vertex) (seg0.containsChild(unsharedVert0) ?
+								seg0.getChild(unsharedVert0) : seg0.getChild(unsharedVert1));
+						third = (Vertex) (first.getNameChar() == unsharedVert0.charAt(0) ?
+								seg1.getChild(unsharedVert1) : seg1.getChild(unsharedVert0));
+						second = (Vertex) seg0.getChild(sharedVert);
+						Angle newAngle = new Angle(first, second, third);
+						angles.add(newAngle);
+					}
 				}
 			}
 		}
-		return new SimpleEntry<>(segs, angles);
+		return Arrays.asList(segs, angles);
 	}
 	
 	private Vertex[] getFarthestVertices(List<Vertex> vertices) {
