102e93ab965215aafed73230770642b31caf07a5
DavidDinkevich <david.dinkevich@gmail.com>
2017-10-07 22:46:15 -0400

    GraphicsRectEllipse now extends GraphicsShape (not GraphicsShape2D)
    (GraphicsShape2D has been deleted)

--- Proofer/src/ui/canvas/GraphicsRectEllipse.java
+++ Proofer/src/ui/canvas/GraphicsRectEllipse.java
@@ -6,7 +6,7 @@ import geometry.shapes.RectEllipse;
  * A {@link GraphicsShape2D} that represents {@link RectEllipse}s.
  * @author David Dinkevich
  */
-public abstract class GraphicsRectEllipse<T extends RectEllipse> extends GraphicsShape2D<T> {
+public abstract class GraphicsRectEllipse<T extends RectEllipse> extends GraphicsShape<T> {
 	public GraphicsRectEllipse(Brush brush, T re) {
 		super(brush, re);
 	}
