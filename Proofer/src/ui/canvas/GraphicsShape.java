48afddb125f8139f484bdcfa1ba8151a0d431f94
DavidDinkevich <david.dinkevich@gmail.com>
2017-10-07 22:45:21 -0400

    GraphicsShape now overrides methods of Selectable interface

--- Proofer/src/ui/canvas/GraphicsShape.java
+++ Proofer/src/ui/canvas/GraphicsShape.java
@@ -1,10 +1,11 @@
 package ui.canvas;
 
+import exceptions.IllegalSelectionException;
 import geometry.shapes.AbstractShape;
 import geometry.shapes.Shape;
 
 import ui.canvas.selection.Selectable;
-
+import ui.canvas.selection.Selector;
 import util.IdentifiableObject;
 
 /**
@@ -14,9 +15,15 @@ import util.IdentifiableObject;
 public abstract class GraphicsShape<T extends Shape>
 extends IdentifiableObject
 implements Drawable, Selectable {	
+	
+	public static final String LAYER_NAME = "shapes";
 
 	private T shape;
 	private Brush.Builder brush;
+	// By default, GraphicsShapes are NOT selectable
+	private boolean allowSelections;
+	private boolean selected;
+	private Selector<?, ?> selector;
 	
 	// The Layer to which this object belongs
 	private String layer = LayerManager.DEFAULT_LAYER;
@@ -37,6 +44,7 @@ implements Drawable, Selectable {
 	public GraphicsShape(GraphicsShape<T> o) {
 		brush = new Brush.Builder(o.brush.buildBrush());
 		layer = o.layer;
+		allowSelections = o.allowSelections;
 	}
 	
 	@Override
@@ -67,5 +75,47 @@ implements Drawable, Selectable {
 	}
 	public void setLayer(String layer) {
 		this.layer = layer;
+	}
+	
+	@Override
+	public boolean getAllowSelections() {
+		return allowSelections;
+	}
+	
+	@Override
+	public void setAllowSelection(boolean selectable) {
+		this.allowSelections = selectable;
+	}
+	
+	@Override
+	public boolean isSelected() {
+		return selected;
+	}
+	
+	@Override
+	public void selectWithSelector(Selector<?, ?> sel) {
+		if (!allowSelections) {
+			throw new IllegalSelectionException("Cannot select a selectable object"
+					+ "that does not allow selections.");
+		}
+		selected = true;
+		selector = sel;
+	}
+	
+	@Override
+	public Selector<?, ?> deselect() {
+		selected = false;
+		Selector<?, ?> sel = selector;
+		selector = null;
+		return sel;
+	}
+	
+	@Override
+	public Selector<?, ?> getSelector() {
+		if (!allowSelections) {
+			throw new IllegalSelectionException("Cannot get selector from a selectable object "
+					+ "that does not allow selections.");
+		}
+		return selector;
 	}
 }
