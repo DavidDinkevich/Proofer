49c4bffd21c728d6cc12708fb9f37de8e24d3fe9
DavidDinkevich <david.dinkevich@gmail.com>
2017-10-22 22:59:16 -0400

    WIP: making class to graphically represent component of GraphicsPolygons

---
+++ Proofer/src/ui/canvas/GraphicsPolygonComponent.java
@@ -0,0 +1,29 @@
+package ui.canvas;
+
+/**
+ * Represents a {@link Segment} or {@link Amgle} in a {@link GraphicsPolygon}. 
+ * Can be rendered to the screen by the {@link Drawable#draw(Canvas)} method.
+ * @author David Dinkevich
+ */
+public class GraphicsPolygonComponent implements Drawable {
+	private GraphicsTriangle parentTri;
+	private String componentName;
+	
+	public GraphicsPolygonComponent(GraphicsTriangle tri, String componentName) {
+		parentTri = tri;
+		this.componentName = componentName;
+	}
+	
+	@Override
+	public void draw(Canvas c) {
+		
+	}
+	
+	public String getComponentName() {
+		return componentName;
+	}
+	
+	public GraphicsTriangle getParentPolygon() {
+		return parentTri;
+	}
+}
