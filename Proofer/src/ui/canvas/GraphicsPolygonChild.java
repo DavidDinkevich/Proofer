bc76608013511fe7bb7b9901bfd2495d26306db4
DavidDinkevich <david.dinkevich@gmail.com>
2017-10-24 14:05:57 -0400

    GraphicsPolygonChild now extends GraphicsShape

--- Proofer/src/ui/canvas/GraphicsPolygonChild.java
+++ Proofer/src/ui/canvas/GraphicsPolygonChild.java
@@ -1,29 +1,66 @@
 package ui.canvas;
 
+import geometry.shapes.Arc;
+import geometry.shapes.Shape;
+
 /**
  * Represents a child in a {@link GraphicsPolygon}. 
  * Can be rendered to the screen by the {@link Drawable#draw(Canvas)} method.
  * @author David Dinkevich
  */
-public class GraphicsPolygonChild implements Drawable {
+public class GraphicsPolygonChild extends GraphicsShape<Shape> {
 	private GraphicsTriangle parentTri;
-	private String childName;
 	
-	public GraphicsPolygonChild(GraphicsTriangle tri, String childName) {
+	public GraphicsPolygonChild(Brush brush, GraphicsTriangle tri) {
 		parentTri = tri;
-		this.childName = childName;
+	}
+	
+	public GraphicsPolygonChild(GraphicsTriangle poly) {
+		this(new Brush(), poly);
+	}
+	
+	public GraphicsPolygonChild(Brush brush) {
+		this(brush, new GraphicsTriangle());
+	}
+	
+	public GraphicsPolygonChild() {
+		this(new Brush());
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (super.equals(o))
+			return true;
+		if (!(o instanceof GraphicsPolygonChild))
+			return false;
+		GraphicsPolygonChild p = (GraphicsPolygonChild)o;
+		return parentTri.equals(p.parentTri);
+	}
+	
+	@Override
+	public int hashCode() {
+		int result = super.hashCode();
+		result = 31 * result + parentTri.hashCode();
+		return result;
 	}
 	
 	@Override
 	public void draw(Canvas c) {
-		
-	}
-	
-	public String getChildName() {
-		return childName;
+		super.draw(c);
+		Shape newShape = parentTri.getShapeOfChild(getShape().getName());
+		if (newShape != null) {
+			setShape(newShape);
+			if (getShape() instanceof Arc) {
+				c.arc((Arc)getShape());
+			}
+		}
 	}
 	
 	public GraphicsTriangle getParentPolygon() {
 		return parentTri;
+	}
+	
+	public void setParentPolygon(GraphicsTriangle poly) {
+		parentTri = poly;
 	}
 }
