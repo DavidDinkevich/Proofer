5099b6d5b1cb476947c7c53adb8ebe5ff33f602a
DavidDinkevich <david.dinkevich@gmail.com>
2017-11-30 23:02:51 -0500

    GraphicsPolygonAngle: work in progress--overriding getShape()

--- Proofer/src/ui/canvas/GraphicsPolygonAngle.java
+++ Proofer/src/ui/canvas/GraphicsPolygonAngle.java
@@ -1,10 +1,15 @@
 package ui.canvas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import geometry.Dimension;
+import geometry.Vec2;
 import geometry.shapes.Angle;
 import geometry.shapes.Arc;
 import geometry.shapes.Segment;
 import geometry.shapes.Shape;
+import geometry.shapes.Vertex;
 
 import util.Utils;
 
@@ -38,6 +43,16 @@ public class GraphicsPolygonAngle extends GraphicsPolygonChild<Angle> {
 		return name;
 	}
 	
+	@Override
+	public Angle getShape() {
+		Angle copy = new Angle(super.getShape());
+		Vec2 loc0 = copy.getVertices().get(0).getCenter(true);
+		copy.getVertices().get(0).setCenter(loc0.valueAtMag(0.4f), true);
+		Vec2 loc1 = copy.getVertices().get(2).getCenter(true);
+		copy.getVertices().get(2).setCenter(loc1.valueAtMag(0.4f), true);
+		return copy;
+	}
+	
 	/**
 	 * The {@link Shape} of this {@link GraphicsPolygonAngle} is an {@link Angle}.
 	 * However, when we draw this object to a {@link Canvas}, we want to draw an
@@ -46,7 +61,7 @@ public class GraphicsPolygonAngle extends GraphicsPolygonChild<Angle> {
 	 */
 	private Arc getArcShape() {
 		// Get the name of the center vertex (it's in sync with the poly's name)
-		String vertName = getShape().getChildren().get(1).getName();
+		String vertName = super.getShape().getChildren().get(1).getName();
 		// Get segments adjacent to vertex
 		Segment[] adjSegs = getParentPolygon().getShape().getAdjacentSegments(vertName);
 		// Lengths of segments
@@ -57,7 +72,7 @@ public class GraphicsPolygonAngle extends GraphicsPolygonChild<Angle> {
 		// The size of the arc: shorter segment * 0.2
 		final float arcSize = Math.min(s0Len, s1Len) * fraction;
 		// Derive arc
-		return Utils.getArc(getShape(), new Dimension(arcSize));
+		return Utils.getArc(super.getShape(), new Dimension(arcSize));
 	}
 	
 	@Override
