0f4c7e5b909d110fc689a9ec63649bb9973ff02c
DavidDinkevich <david.dinkevich@gmail.com>
2019-01-18 21:12:59 -0500

    Initial commit

---
+++ Proofer/src/geometry/proofs/PerpendicularFigureRelation.java
@@ -0,0 +1,37 @@
+package geometry.proofs;
+
+import geometry.shapes.Segment;
+
+public class PerpendicularFigureRelation extends FigureRelation {
+
+	private char intersectVert;
+	
+	public PerpendicularFigureRelation(Segment fig0, Segment fig1, 
+			FigureRelation parent, char intersectVert) {
+		
+		super(FigureRelationType.PERPENDICULAR, fig0, fig1, parent);
+		this.intersectVert = intersectVert;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (!super.equals(o))
+			return false;
+		if (!(o instanceof PerpendicularFigureRelation))
+			return false;
+		PerpendicularFigureRelation other = (PerpendicularFigureRelation) o;
+		return other.intersectVert == intersectVert;
+	}
+	
+	@Override
+	public int hashCode() {
+		int result = super.hashCode();
+		result = 31 * result + Character.hashCode(intersectVert);
+		return result;
+	}
+	
+	public char getIntersectVert() {
+		return intersectVert;
+	}
+	
+}
