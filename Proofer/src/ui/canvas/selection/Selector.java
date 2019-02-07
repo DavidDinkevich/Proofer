4db8272fdd39fc9eacfe16d98e51d4e2863a5215
DavidDinkevich <david.dinkevich@gmail.com>
2019-02-07 13:09:32 -0500

    Initial commit of new Selector class

---
+++ Proofer/src/ui/canvas/selection/Selector.java
@@ -0,0 +1,102 @@
+package ui.canvas.selection;
+
+import geometry.Vec2;
+import geometry.shapes.VertexShape;
+
+import ui.canvas.AdvancedCanvas;
+import ui.canvas.GraphicsEllipse;
+import ui.canvas.GraphicsShape;
+import ui.canvas.StyleManager;
+import ui.canvas.diagram.UIDiagramLayers;
+
+public class Selector extends GraphicsShape<VertexShape> {
+	
+	private Knob[] knobs;
+	private GraphicsShape<? extends VertexShape> target;
+
+	public Selector(GraphicsShape<? extends VertexShape> target) {
+		super(target.getShape());
+		this.target = target;
+		target.setSelected(true);
+		
+		setLayer(UIDiagramLayers.SELECTOR);
+
+		// Create knobs
+		knobs = new Knob[target.getShape().getVertexCount()];
+		for (int i = 0; i < knobs.length; i++) {
+			knobs[i] = new Knob(this, target.getShape().getVertexName(i));
+		}
+		
+		updateKnobPositions();
+	}
+	
+	@Override
+	public void draw(AdvancedCanvas c) {
+		// Draw knobs
+		for (Knob knob : knobs) {
+			knob.draw(c);
+		}
+	}
+	
+	/**
+	 * Return each {@link Knob} to its default position on the {@link Selector}.
+	 * "Default position" = the position that the knob ideally should be
+	 * on this selector.
+	 */
+	public void updateKnobPositions() {
+		for (int i = 0; i < getKnobs().length; i++) {
+			// TODO: change target to getshape to test that the shape is moving
+			// with target
+			getKnobs()[i].getShape().setCenter(target.getShape().getVertexLoc(i));
+		}
+	}
+	
+	public void setSelectorLoc(Vec2 loc) {
+		target.getShape().setCenter(loc);
+		updateKnobPositions();
+	}
+	
+	public void deselectTarget() {
+		target.setSelected(false);
+		knobs = null;
+		target = null;
+	}
+	
+	public Knob[] getKnobs() {
+		return knobs;
+	}
+	
+	public GraphicsShape<? extends VertexShape> getTarget() {
+		return target;
+	}
+	
+	public static class Knob extends GraphicsEllipse {
+
+		private Selector sel;
+		private char controlledVertex;
+		
+		public Knob(Selector sel, char controlledVertex) {
+			super(new GraphicsEllipse(StyleManager.getKnobBody()));
+			setLayer(UIDiagramLayers.KNOB);
+			// TODO:
+//			setAllowSelection(false);
+			getShape().setName("" + controlledVertex);
+			this.controlledVertex = controlledVertex;
+			this.sel = sel;
+		}
+		
+		public void moveKnob(Vec2 newLoc) {
+			sel.getShape().setVertexLoc(controlledVertex, newLoc);
+			getShape().setCenter(newLoc);
+		}
+		
+		public Selector getSelector() {
+			return sel;
+		}
+		
+		public char getControlledVertex() {
+			return controlledVertex;
+		}
+	}
+
+}
