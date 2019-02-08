a439e4b5a2fb467c0727809389cfbd39e430d640
DavidDinkevich <david.dinkevich@gmail.com>
2019-02-08 12:44:58 -0500

    DiagramCanvas is now responsible for rendering invisible hidden triangles
    It is also now a VertexBufferListener

--- Proofer/src/ui/canvas/diagram/DiagramCanvas.java
+++ Proofer/src/ui/canvas/diagram/DiagramCanvas.java
@@ -8,17 +8,25 @@ import javafx.scene.paint.Color;
 
 import geometry.Dimension;
 import geometry.Vec2;
+import geometry.proofs.Diagram;
 import geometry.proofs.Figure;
+import geometry.proofs.Preprocessor;
+import geometry.shapes.Angle;
+import geometry.shapes.Triangle;
 import geometry.shapes.VertexBuffer;
+import geometry.shapes.VertexBufferListener;
 import geometry.shapes.VertexShape;
 
 import ui.canvas.AdvancedCanvas;
+import ui.canvas.Drawable;
 import ui.canvas.GraphicsShape;
+import ui.canvas.GraphicsTriangle;
 import ui.canvas.GraphicsVertexBuffer;
+import ui.canvas.StyleManager;
 import ui.canvas.selection.InputManager;
 
 
-public class DiagramCanvas extends AdvancedCanvas {
+public class DiagramCanvas extends AdvancedCanvas implements VertexBufferListener {
 	
 	public enum UIDiagramLayers {
 		DEFAULT, GRID, INVISIBLE_HIDDEN_FIGURES, 
@@ -39,6 +47,7 @@ public class DiagramCanvas extends AdvancedCanvas {
 		diagramFigures = new ArrayList<>();
 		renderList = new RenderList();
 		vertexBuff = new VertexBuffer();
+		vertexBuff.getListeners().add(this);
 		inputManager = new InputManager(this);
 		
 		// Add layers to render list
@@ -63,7 +72,7 @@ public class DiagramCanvas extends AdvancedCanvas {
 		inputManager.draw(this);
 
 	}
-	
+		
 	public void addDiagramFigure(GraphicsShape<?> shape) {
 		// Add to list to diagram figures list
 		diagramFigures.add(shape);
@@ -124,6 +133,125 @@ public class DiagramCanvas extends AdvancedCanvas {
 		return false;
 	}
 	
+	@Override
+	public void vertexNameChanged(char oldName, char newName) {
+		System.out.println("Old name new name: " + oldName + ", " + newName);
+		
+		// Highlight or unhighlight invisible hidden figures
+		updateInvisibleHiddenFigures();
+
+	}
+	
+	/*
+	 * BEGIN HIDDEN INVISIBLE FIGURES
+	 */
+	
+	private void updateInvisibleHiddenFigures() {
+		/*
+		 * Update list of invisible hidden figures
+		 */
+		
+		// Get list of all figures, included invisible hidden ones
+		Diagram diag = Preprocessor.compileFigures(this);
+		
+		// Remove outdated figures
+		removeOutdatedHiddenInvisibleFigures(diag);
+		
+		// Triangles
+		for (Figure fig : diag.getFigures()) {
+			// Make sure it's a triangle
+			if (fig.getClass() != Triangle.class)
+				continue;
+			
+			// Add the hidden invisible figure
+			if (isHiddenInvisibleFigure(diag, fig.getName())) {
+				// Get the invisible triangle, and make a GraphicsShape for it
+				Triangle tri = (Triangle) fig;
+				GraphicsTriangle gtri = new GraphicsTriangle(tri);
+				gtri.setBrush(StyleManager.getInvisibleHiddenFigureBrush());
+				gtri.setLayer(UIDiagramLayers.INVISIBLE_HIDDEN_FIGURES);
+				renderList.addDrawable(gtri);
+			} 
+		}
+	}
+	
+	private boolean isHiddenInvisibleFigure(Diagram diag, String name) {
+		// If the figure is NOT contained in the Canvas, then it is a hidden figure
+		// so we'll add it to the canvas (UNLESS it has been already added, in which
+		// case we don't want duplicates). Finally, we don't want to add
+		// "secondary triangles" (see isSecondaryTriangle() for description). Doing so
+		// would result in ugly triangles being created on top of other ones.
+		return !containsDiagramFigure(name, Triangle.class)
+		&& !containsHiddenInvisibleFigure(name)
+		&& !isSecondaryTriangle(diag, name);
+	}
+	
+	private boolean containsHiddenInvisibleFigure(String name) {
+		// Get list of invisible hidden figures
+		List<Drawable> invisFigs = renderList.
+				getLayerList(UIDiagramLayers.INVISIBLE_HIDDEN_FIGURES);
+
+		for (Drawable drawable : invisFigs) {
+			// We know it's a GraphicsShape because we're the ones who add all the
+			// invisible hidden figures and we only add GraphicsShapes
+			GraphicsShape<?> invisFigure = (GraphicsShape<?>) drawable;
+			if (invisFigure.getShape().isValidName(name))
+				return true;
+		}
+		
+		return false;
+	}
+	
+	private void removeOutdatedHiddenInvisibleFigures(Diagram diag) {
+		// Get list of invisible hidden figures
+		List<Drawable> invisFigs = renderList.
+				getLayerList(UIDiagramLayers.INVISIBLE_HIDDEN_FIGURES);
+		
+		// Remove invisible hidden figures that may have existed in the
+		// past but no longer exist. Such figures will exist within the
+		// invisible hidden figures list but not in the diagram
+		for (int i = invisFigs.size() - 1; i >= 0; i--) {
+			// We know it's a GraphicsShape because we're the ones who add all the
+			// invisible hidden figures and we only add GraphicsShapes
+			GraphicsShape<?> invisFigure = (GraphicsShape<?>) invisFigs.get(i);
+			// Get the type of the figure--cast is safe because we know the type
+			// extends Figure
+			@SuppressWarnings("unchecked")
+			Class<? extends Figure> type = (Class<? extends Figure>) 
+					invisFigure.getShape().getClass();
+			
+			if (!diag.containsFigure(invisFigure.getShape().getName(), type)) {
+				// Remove from render list
+				renderList.removeDrawable(invisFigure);
+			}
+		}
+	}
+	
+	/**
+	 * A "secondary triangle" in this context is simply a triangle that is composed
+	 * of one or more angle synonyms.
+	 * @param diag the diagram in which the triangle lies
+	 * @param name the name of the triangle
+	 * @return whether or not it is a "secondary triangle"
+	 */
+	private boolean isSecondaryTriangle(Diagram diag, String name) {
+		Triangle tri = diag.getFigure(name, Triangle.class);
+		if (tri == null) {
+			return false;
+		}
+		
+		for (Angle angle : tri.getAngles()) {
+			if (diag.isSecondaryAngleSynonym(angle.getName()))
+				return true;
+		}
+		return false;
+	}
+	
+	/*
+	 * END HIDDEN INVISIBLE FIGURES
+	 */
+
+	
 	public List<GraphicsShape<?>> getDiagramFigures() {
 		return diagramFigures;
 	}
@@ -143,5 +271,5 @@ public class DiagramCanvas extends AdvancedCanvas {
 	public VertexBuffer getVertexBuffer() {
 		return vertexBuff;
 	}
-	
+
 }
